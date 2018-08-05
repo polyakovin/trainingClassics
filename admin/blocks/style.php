aaf7924a2da16a7fee8caaf27560226ff96cb43f
iGor Polyakov <igor_polyakov@phystech.edu>
2018-08-05 11:02:47 +0300

    project cleared

--- admin/blocks/style.php
+++
@@ -1,8 +0,0 @@
-<link href="css/style.css" rel="stylesheet" type="text/css">
-<link rel="shortcut icon" href="favicon.png" type="image/x-icon">
-<!--[if  lt IE 8]><style type="text/css">#main{margin:0;}</style><![endif]-->
-<link rel="stylesheet" href="css/slimbox2.css" type="text/css" media="screen" />
-<link rel="stylesheet" href="css/jquery-ui-1.8.17.custom.css" type="text/css" media="screen" />
-<script type="text/javascript" src="js/jquery.js"></script>
-<script type="text/javascript" src="js/jquery-ui-1.8.17.custom.min.js"></script>
-<script type="text/javascript" src="js/js.js"></script>
--- admin/js/js.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function(){
-$('input,textarea').focus(function(){$(this).addClass('focus');}).blur(function(){$(this).removeClass('focus');});
-$('#date').datepicker({minDate:0,changeMonth:true,changeYear:true});
-}); //Конец ready
